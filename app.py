ed04f657d8667f98d921b4e071a0f6f21a87bf30
bhagyabejju <148511552+bhagyabejju@users.noreply.github.com>
2023-10-20 07:57:36 +0530

    i have addded my name in this file

--- app.py
+++ app.py
@@ -3,10 +3,7 @@
 x = 5
 y = 10
 
-# To take inputs from the user
-#x = input('Enter value of x: ')
-#y = input('Enter value of y: ')
-
+My name is Mustafa
 # create a temporary variable and swap the values
 temp = x
 x = y
